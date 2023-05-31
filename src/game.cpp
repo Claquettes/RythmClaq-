321afeb4ae5c0e2b08824d3d226ef16df0a6b906
Claquettes <math@claq.fr>
2023-05-31 09:52:52 +0200

    created game init and gameloop

--- src/game.cpp
+++ src/game.cpp
@@ -8,10 +8,62 @@
 Game::Game()
 {
     std::cout << "Game constructor called!" << std::endl;
-
+    // we call the init method
+    init();
+    // we call the gameLoop method once the game is initialized
+    while (!readyToStart)
+    {
+        std::cout << "Initializing game..." << std::endl;
+    }
+    gameLoop();
 }
 
 Game::~Game() {
     // destructor implementation
     std::cout<< "Game destructor called!" << std::endl;
+}
+
+int Game::init(){ 
+    // we initialize the SDL library
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        std::cout << "SDL could not be initialized! SDL_Error: " << SDL_GetError() << std::endl;
+    }
+    //we create the window
+    window = SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+    //we create the renderer
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    //we set the color of the renderer to white
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    readyToStart = true;
+    return 0;
+}
+
+int Game::gameLoop(){
+    // main loop flag
+    bool quit = false;
+    // event handler
+    SDL_Event e;
+    // main loop
+    while (!quit)
+    {
+        // event handling
+        while (SDL_PollEvent(&e) != 0)
+        {
+            // user requests quit
+            if (e.type == SDL_QUIT)
+            {
+                quit = true;
+            }
+        }
+        // clear the screen
+        SDL_RenderClear(renderer);
+        // update the screen
+        SDL_RenderPresent(renderer);
+    }
+    // free resources
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
 }
--- src/game.h
+++ src/game.h
@@ -8,15 +8,27 @@
 #include <SDL2/SDL_image.h>
 
 class Game {
-public:
-    // size of the window
-     const int WINDOW_WIDTH = 640;
-     
-    // constructor
-    Game();
+    public:
+        // size of the window
+        const int WINDOW_WIDTH = 640;
+        const int WINDOW_HEIGHT = 480;
 
-    // destructor
-    ~Game();
+        // constructor
+        Game();
+
+        // destructor
+        ~Game();
+
+        // methods
+        int init();
+        int gameLoop();
+    private:
+        bool readyToStart = false;
+        // the window we'll be rendering to
+        SDL_Window* window;
+        SDL_Renderer* renderer;
+
 };
 
 #endif // GAME_H
+
