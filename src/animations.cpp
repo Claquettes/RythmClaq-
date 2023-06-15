7a134c31a1bb9540f03e7e02bc8b9185e131f702
Claquettes <math@claq.fr>
2023-06-15 20:11:25 +0200

    more fade animations

--- src/animations.cpp
+++ src/animations.cpp
@@ -69,3 +69,119 @@ void Animations::changeColor(Pannel& pannel, SDL_Color targetColor, int duration
 
     pannel.setColor(targetColor);
 }
+
+void Animations::fadeIn(Pannel& pannel, int duration, SDL_Renderer* renderer) {
+    SDL_Color startColor = pannel.getColor();
+    int totalFrames = duration / 16; 
+    int frameCount = 0;
+
+    for (int i = 0; i < totalFrames; i++) {
+        float progress = static_cast<float>(i) / totalFrames;
+        Uint8 r = static_cast<Uint8>(startColor.r + (255 - startColor.r) * progress);
+        Uint8 g = static_cast<Uint8>(startColor.g + (255 - startColor.g) * progress);
+        Uint8 b = static_cast<Uint8>(startColor.b + (255 - startColor.b) * progress);
+
+        pannel.setColor({ r, g, b });
+
+        pannel.render(renderer);
+        SDL_RenderPresent(renderer);
+
+        SDL_Delay(16); // Delay for 16ms (60 FPS)
+    }
+
+    pannel.setColor({ 255, 255, 255 });
+}
+
+void Animations::fadeOut(Pannel& pannel, int duration, SDL_Renderer* renderer) {
+    SDL_Color startColor = pannel.getColor();
+    int totalFrames = duration / 16; 
+    int frameCount = 0;
+
+    for (int i = 0; i < totalFrames; i++) {
+        float progress = static_cast<float>(i) / totalFrames;
+        Uint8 r = static_cast<Uint8>(startColor.r + (0 - startColor.r) * progress);
+        Uint8 g = static_cast<Uint8>(startColor.g + (0 - startColor.g) * progress);
+        Uint8 b = static_cast<Uint8>(startColor.b + (0 - startColor.b) * progress);
+
+        pannel.setColor({ r, g, b });
+
+        pannel.render(renderer);
+        SDL_RenderPresent(renderer);
+
+        SDL_Delay(16); // Delay for 16ms (60 FPS)
+    }
+
+    pannel.setColor({ 0, 0, 0 });
+}
+
+void Animations::fadeInOut(Pannel& pannel, int duration, SDL_Renderer* renderer) {
+    SDL_Color startColor = pannel.getColor();
+    int totalFrames = duration / 16; 
+    int frameCount = 0;
+
+    for (int i = 0; i < totalFrames; i++) {
+        float progress = static_cast<float>(i) / totalFrames;
+        Uint8 r = static_cast<Uint8>(startColor.r + (255 - startColor.r) * progress);
+        Uint8 g = static_cast<Uint8>(startColor.g + (255 - startColor.g) * progress);
+        Uint8 b = static_cast<Uint8>(startColor.b + (255 - startColor.b) * progress);
+
+        pannel.setColor({ r, g, b });
+
+        pannel.render(renderer);
+        SDL_RenderPresent(renderer);
+
+        SDL_Delay(16); // Delay for 16ms (60 FPS)
+    }
+
+    for (int i = 0; i < totalFrames; i++) {
+        float progress = static_cast<float>(i) / totalFrames;
+        Uint8 r = static_cast<Uint8>(255 - (255 * progress));
+        Uint8 g = static_cast<Uint8>(255 - (255 * progress));
+        Uint8 b = static_cast<Uint8>(255 - (255 * progress));
+
+        pannel.setColor({ r, g, b });
+
+        pannel.render(renderer);
+        SDL_RenderPresent(renderer);
+
+        SDL_Delay(16); // Delay for 16ms (60 FPS)
+    }
+
+    pannel.setColor({ 0, 0, 0 });
+}
+
+void Animations::fadeOutIn(Pannel& pannel, int duration, SDL_Renderer* renderer) {
+    SDL_Color startColor = pannel.getColor();
+    int totalFrames = duration / 16; 
+    int frameCount = 0;
+
+    for (int i = 0; i < totalFrames; i++) {
+        float progress = static_cast<float>(i) / totalFrames;
+        Uint8 r = static_cast<Uint8>(startColor.r + (0 - startColor.r) * progress);
+        Uint8 g = static_cast<Uint8>(startColor.g + (0 - startColor.g) * progress);
+        Uint8 b = static_cast<Uint8>(startColor.b + (0 - startColor.b) * progress);
+
+        pannel.setColor({ r, g, b });
+
+        pannel.render(renderer);
+        SDL_RenderPresent(renderer);
+
+        SDL_Delay(16); // Delay for 16ms (60 FPS)
+    }
+
+    for (int i = 0; i < totalFrames; i++) {
+        float progress = static_cast<float>(i) / totalFrames;
+        Uint8 r = static_cast<Uint8>(0 + (255 * progress));
+        Uint8 g = static_cast<Uint8>(0 + (255 * progress));
+        Uint8 b = static_cast<Uint8>(0 + (255 * progress));
+
+        pannel.setColor({ r, g, b });
+
+        pannel.render(renderer);
+        SDL_RenderPresent(renderer);
+
+        SDL_Delay(16); // Delay for 16ms (60 FPS)
+    }
+
+    pannel.setColor({ 255, 255, 255 });
+}
--- src/animations.h
+++ src/animations.h
@@ -11,6 +11,10 @@ public:
     static void movePannel(Pannel& Pannel, int targetX, int targetY, int duration, SDL_Renderer* renderer);
     static void resizePannel(Pannel& Pannel, int targetWidth, int targetHeight, int duration, SDL_Renderer* renderer);
     static void changeColor(Pannel& Pannel, SDL_Color targetColor, int duration, SDL_Renderer* renderer);
+    static void fadeIn(Pannel& Pannel, int duration, SDL_Renderer* renderer);
+    static void fadeOut(Pannel& Pannel, int duration, SDL_Renderer* renderer);
+    static void fadeInOut(Pannel& Pannel, int duration, SDL_Renderer* renderer);
+    static void fadeOutIn(Pannel& Pannel, int duration, SDL_Renderer* renderer);    
 };
 
 #endif // ANIMATIONS_H
